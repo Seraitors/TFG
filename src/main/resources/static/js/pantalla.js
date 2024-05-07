dcb86d5038c743d064bde29a144cc30eb169594a
Root <seraitors@gmail.com>
2024-05-07 19:36:10 +0200

    carrusel arreglado

--- src/main/resources/static/js/pantalla.js
+++ src/main/resources/static/js/pantalla.js
@@ -1,6 +1,9 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.carrusel-slide');
 const totalSlides = slides.length;
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let intervalId;
 
 function showSlide(index) {
   slides.forEach((slide) => {
@@ -14,5 +17,22 @@ function nextSlide() {
   showSlide(currentSlide);
 }
 
-// Para avanzar automáticamente al siguiente slide cada cierto tiempo
-setInterval(nextSlide, 3000);
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+nextBtn.addEventListener('click', () => {
+  nextSlide();
+  clearInterval(intervalId); // Stop automatic slide change when manually changing slides
+  intervalId = setInterval(nextSlide, 3000); // Restart automatic slide change after manual change
+});
+
+prevBtn.addEventListener('click', () => {
+  prevSlide();
+  clearInterval(intervalId); // Stop automatic slide change when manually changing slides
+  intervalId = setInterval(nextSlide, 3000); // Restart automatic slide change after manual change
+});
+
+// Automatic slide change every 3 seconds
+intervalId = setInterval(nextSlide, 3000);
