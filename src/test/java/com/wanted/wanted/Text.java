af91f9d302561a5407ef2203f8ea4b87dab91567
seraitors <seraitors@gmail.com>
2024-06-15 21:44:36 +0200

    test

--- src/test/java/com/wanted/wanted/Text.java
+++ src/test/java/com/wanted/wanted/Text.java
@@ -11,11 +11,13 @@ import com.wanted.wanted.entidades.Arco;
 import com.wanted.wanted.entidades.Figura;
 import com.wanted.wanted.repositorio.ArcoRepository;
 import com.wanted.wanted.repositorio.FiguraRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -24,15 +26,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @Slf4j
-@DataJpaTest
-@DBRider
-@ExtendWith(SpringExtension.class)
-
+@Transactional
+@SpringBootTest
 public class Text {
 
 
@@ -46,206 +45,27 @@ public class Text {
 
         @Autowired
         private ArcoRepository arcoRepository;
-        public static final int PAGE_SIZE = 2;
-
-
     @Test
-    void testFindByNombreStartingWithIgnoreCase() {
-        List<Figura> figuras = figuraRepository.findByNombreStartingWithIgnoreCase("Figura");
-        assertThat(figuras).isNotEmpty();
-    }
-
-    @Test
-    void testFindByNombreContainsIgnoreCase() {
-        List<Figura> figuras = figuraRepository.findByNombreContainsIgnoreCase("Figura");
-        assertThat(figuras).isNotEmpty();
-    }
-
-    @Test
-    void testFindByNombre() {
-        String nombre = "Figura";
-        List<Figura> figuras = figuraRepository.findByNombre(nombre);
-        assertThat(figuras).isNotEmpty();
-    }
-
-    @Test
-    void testFindFigurasByPrecioGreaterThan() {
-        int precio = 100;
-        List<Figura> figuras = figuraRepository.findFigurasByPrecioGreaterThan(precio);
-        assertThat(figuras).isNotEmpty();
-    }
-
-    @Test
-    void testFindFigurasByPrecio() {
-        int precio = 50;
-        List<Figura> figuras = figuraRepository.findFigurasByPrecio(precio);
-        assertThat(figuras).isNotEmpty();
-    }
-
-    @Test
-    void testFindFigurasByNombre() {
-        String nombre = "Nombre";
-        List<Figura> figuras = figuraRepository.findFigurasByNombre(nombre);
-        assertThat(figuras).isNotEmpty();
-    }
-
-    @Test
-    void testFindFigurasByDes() {
-        String des = "Descripción";
-        List<Figura> figuras = figuraRepository.findFigurasByDes(des);
-        assertThat(figuras).isNotEmpty();
-    }
-
-//    @Test
-//    void testFindAllByOrderByNombreAsc() {
-//        List<Figura> figuras = figuraRepository.findAllByOrderByNombreAsc();
-//        assertThat(figuras).isNotEmpty();
-//    }
-//
-//    @Test
-//    void testFindTop3By() {
-//        List<Figura> figuras = figuraRepository.findTop3By();
-//        assertThat(figuras).isNotEmpty();
-//    }
-//    @Test
-//    public void testAddFigura() {
-//        Figura figura = new Figura();
-//        figura.setNombre("Luffy");
-//        figura.setUrl("/imagen/fotosLista/luffy.jpg");
-//
-//        when(figuraRepository.save(any(Figura.class))).thenReturn(figura);
-//
-//        Figura savedFigura = figuraService.add(figura);
-//
-//        assertNotNull(savedFigura);
-//        assertEquals("Luffy", savedFigura.getNombre());
-//        verify(figuraRepository, times(1)).save(figura);
-//    }
-
-    @Test
-    public void testFindFiguraById() {
+    public void testFindByNombre() {
         Figura figura = new Figura();
-        figura.setId(1L);
-        figura.setNombre("Luffy");
+        figura.setNombre("Obito");
+        figuraRepository.save(figura);
 
-        when(figuraRepository.findById(1L)).thenReturn(Optional.of(figura));
-
-        Optional<Figura> foundFigura = figuraRepository.findById(1L);
-
-        assertTrue(foundFigura.isPresent());
-        assertEquals("Luffy", foundFigura.get().getNombre());
+        List<Figura> result = figuraRepository.findByNombre("Obito");
+        assertEquals(1, result.size());
+        assertEquals("Obito", result.get(0).getNombre());
     }
 
-        /*//derivada
-        @Test
-
-        void testConsultasDerivadas() {
-            List<Arco> arco = arcoRepository.findByNombre("Wano");
-            assertThat(arco).hasSize(1);
-        }
-
-
-
-
-
-
-
-        //by example
-        @Test
-        public void testQueryByExample() {
-
-            Figura ejemploPersona = new Figura();
-            ejemploPersona.setNombre("Zoro");
-
-
-            ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.STARTING);
-
-            Example<Figura> example = Example.of(ejemploPersona, matcher);
-
-            List<Figura> resultado = figuraRepository.findAll(example);
-            System.out.println(resultado);
-        }
-
-
-
-
-
-
-
-        //query
-        @Test
-        @DataSet(value = {"datasets/persona.yml", "datasets/tipo_arco.yml"})
-        public void TesDeQuery(){
-            List<Figura> Figuras = figuraRepository.findPersonasByPrecio(90);
-
-            assertThat(Figuras.get(0).getPrecio()).isEqualTo(90);
-
-        }
-
-
-
-
-        //ordenar
-        @Test
-        @DataSet(value = {"datasets/persona.yml", "datasets/tipo_arco.yml"})
-        public  void Ordenar(){
-            Sort sort = Sort.by(Sort.Order.asc("nombre"));
-
-
-            List<Figura> personas = figuraRepository.findAllByOrderByNombreAsc(sort);
-            assertEquals("Kaido",personas.get(0).getNombre());
-        }
-
-
-        @Test
-        @DataSet(value = {"datasets/persona.yml", "datasets/tipo_arco.yml"})
-        void should_FindByNombreStartingWithIgnoreCase() {
-            // Aquí se asume que tienes una clase PersonaRepository para gestionar entidades Persona
-            List<Figura> personas = figuraRepository.findByNombreContainsIgnoreCase("l");
-            int numFiguras = 1;
-            assertThat(personas).hasSize(numFiguras);
-        }
-
-
-        // busca entre dos precios y si lo encuentra te dice qu eets atood bien
-        @Test
-
-        void testFindAllWithCriteria_DosFechas() {
-            Integer fechaFrom = 90;
-            Integer fechaTo = 100;
-            List<Figura> personas = figuraRepository.findAllWithCriteria(fechaFrom,fechaTo);
-
-            assertThat(personas).extracting(Figura::getId)
-                    .containsExactlyInAnyOrder(DatasetConstants.Luffy_id, DatasetConstants.Zoro_id);
-        }
-
-        @Test
-        void testFindAllWithCriteria_Null(){
-
-            Integer fechaFrom = 150;
-            Integer fechaTo = 200;
-            List<Figura> personas = figuraRepository.findAllWithCriteria(fechaFrom,fechaTo);
-
-            assertThat(personas).extracting(Figura::getId)
-                    .containsExactlyInAnyOrder(DatasetConstants.Sanji_id, DatasetConstants.Kaido_id);
-
-        }
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @Test
+    public void testArcoBuilder() {
+        Arco arco = Arco.builder()
+                .id(1L)
+                .nombre("Pepe")
+                .build();
+        assertNotNull(arco);
+        assertEquals(1L, arco.getId());
+        assertEquals("Pepe", arco.getNombre());
+    }
 
 
 
